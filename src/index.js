f480bbd22a98a597f2b2b1bfc33040c15115c931
Andre Wiggins <andrewiggins@live.com>
2024-04-03 14:15:53 -0700

    Fix typing errors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+/**
+ * @typedef {Required<ExportedHandler<Environment>>} RequiredExportedHandler
+ * @type {{ fetch: RequiredExportedHandler["fetch"] }}
+ */
 export default {
 	async fetch(request, env, ctx) {
 		return new Response("Hello World!");
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,21 @@
+/// <reference types="@cloudflare/vitest-pool-workers" />
+
 import { env, createExecutionContext, waitOnExecutionContext, SELF } from "cloudflare:test";
 import { describe, it, expect } from "vitest";
 import worker from "../src";
 
+/**
+ * @typedef {unknown} CfHostMetadata
+ * @typedef {Request<CfHostMetadata, IncomingRequestCfProperties<CfHostMetadata>>} IncomingRequest
+ * @type {(url: string) => IncomingRequest}
+ */
+function createRequest(url) {
+	return /** @type {Request<CfHostMetadata, IncomingRequestCfProperties<CfHostMetadata>>} */ (new Request(url));
+}
+
 describe("Hello World worker", () => {
 	it("responds with Hello World! (unit style)", async () => {
-		const request = new Request("http://example.com");
+		const request = createRequest("http://example.com");
 		// Create an empty context to pass to `worker.fetch()`.
 		const ctx = createExecutionContext();
 		const response = await worker.fetch(request, env, ctx);
