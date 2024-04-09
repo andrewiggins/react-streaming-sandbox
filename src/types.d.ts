1030b6b3b07a9c061308cb162ab93cd3bf968370
Andre Wiggins <andrewiggins@live.com>
2024-04-09 16:57:52 -0700

    Add debugging config

--- src/types.d.ts
+++ src/types.d.ts
@@ -11,7 +11,37 @@ declare module "*.html" {
 }
 
 declare module "react-dom/server.edge" {
-	export const renderToReadableStream: typeof import("react-dom/server").renderToReadableStream;
+	export interface ScriptConfig {
+		src: string;
+		integrity: string;
+		crossOrigin?: "" | "use-credentials" | "anonymous";
+	}
+
+	export interface RenderToReadableStreamOptions {
+		identifierPrefix?: string;
+		namespaceURI?: string;
+		nonce?: string;
+		bootstrapScriptContent?: string;
+		bootstrapScripts?: string[];
+		bootstrapModules?: string[];
+		progressiveChunkSize?: number;
+		signal?: AbortSignal;
+		onError?: (error: unknown, errorInfo: ErrorInfo) => string | void;
+	}
+
+	export interface ReactDOMServerReadableStream extends ReadableStream {
+		allReady: Promise<void>;
+	}
+
+	/**
+	 * Only available in the environments with [Web Streams](https://developer.mozilla.org/en-US/docs/Web/API/Streams_API) (this includes browsers, Deno, and some modern edge runtimes).
+	 *
+	 * @see [API](https://reactjs.org/docs/react-dom-server.html#rendertoreadablestream)
+	 */
+	export function renderToReadableStream(
+		children: ReactNode,
+		options?: RenderToReadableStreamOptions,
+	): Promise<ReactDOMServerReadableStream>;
 }
 
 interface Assets {
