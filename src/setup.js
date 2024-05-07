fbb6ac5491cad4ded0257215536ea332d249caab
Andre Wiggins <andrewiggins@live.com>
2024-05-07 14:55:51 -0700

    Prefix sessionStorage keys

--- src/setup.js
+++ src/setup.js
@@ -3,31 +3,38 @@ import { createCachedFetch, setMockFetchStore } from "../shared/fetch.js";
 import { RequestController } from "../shared/RequestController.js";
 
 // Use localStorage to map URLs to keys in the cache. Useful for
-// debugging/modifying the cache in the browse without having to enter into the
+// debugging/modifying the cache in the browser without having to enter into the
 // debugger or modify the cache directly.
 const keyMap = sessionStorage;
 /** @type {FetchCache} */
 const cache = new Map();
 
+/** @type {(key: string) => string} */
+const getSessionStorageKey = (key) => `[rss-cache]${key}`;
+
 /** @type {FetchCache} */
 const clientFetchCache = {
 	has(url) {
-		return keyMap.getItem(url) !== null;
+		const ssKey = getSessionStorageKey(url);
+		return keyMap.getItem(ssKey) !== null;
 	},
 	get(url) {
-		const cacheKey = keyMap.getItem(url);
+		const ssKey = getSessionStorageKey(url);
+		const cacheKey = keyMap.getItem(ssKey);
 		if (!cacheKey) return undefined;
 		return cache.get(cacheKey);
 	},
 	set(url, responsePromise) {
+		const ssKey = getSessionStorageKey(url);
 		const cacheKey = crypto.randomUUID();
-		keyMap.setItem(url, cacheKey);
+		keyMap.setItem(ssKey, cacheKey);
 		cache.set(cacheKey, responsePromise);
 	},
 	delete(url) {
+		const ssKey = getSessionStorageKey(url);
 		const cacheKey = keyMap.getItem(url);
 		if (!cacheKey) return;
-		keyMap.removeItem(url);
+		keyMap.removeItem(ssKey);
 		cache.delete(cacheKey);
 	},
 	clear() {
